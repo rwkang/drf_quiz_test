1584615fcf3af2e80a4f6d9d475e49eb8cc1c052
rowonkang <kangrowon@gmail.com>
2021-01-04 14:24:35 +0800

    Ready for deply

---
+++ quiz/views.py
@@ -0,0 +1,16 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Quiz
+from .serializers import QuizSerializer
+import random
+
+# Create your views here.
+@api_view(['GET'])
+def helloAPI(request):
+    return Response("hello World!")
+
+@api_view(['GET'])
+def randomQuiz(request, id):
+    totalQuizs = Quiz.objects.all()
+    randomQuiz = random.sample(list(totalQuizs), id)
+    serializer = QuizSerializer(randomQuiz, many=True)  # 다량의 데이터도 json 직렬화 시킨다.
